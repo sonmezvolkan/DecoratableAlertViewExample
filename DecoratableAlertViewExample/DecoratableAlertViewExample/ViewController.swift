736eeb9bb78e9ff6212bcf75520d0aef5a4b0ccc
Volkan <volkan.sonmez@etts.com.tr>
2020-08-05 19:58:35 +0300

    first commit

---
+++ DecoratableAlertViewExample/DecoratableAlertViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DecoratableAlertViewExample
+//
+//  Created by Volkan Sönmez on 5.08.2020.
+//  Copyright © 2020 Volkan Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
