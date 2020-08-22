3ec56be7b8b72153649d44c2c63ce00f5dfdcfb3
Volkan Sonmez <sonmez.volkan92@gmail.com>
2020-08-22 10:16:47 +0300

    menu view

--- DecoratableAlertViewExample/DecoratableAlertViewExample/DecoratableAlertView/TableViewCell/MenuCell.swift
+++ DecoratableAlertViewExample/DecoratableAlertViewExample/DecoratableAlertView/TableViewCell/MenuCell.swift
@@ -12,9 +12,24 @@ import UIKit
 public class MenuCell: UITableViewCell {
     
     @IBOutlet weak var lblMenu: UILabel!
+    @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var backImage: UIImageView!
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = UIColor(red: 41, green: 80, blue: 251, alpha: 0)
+        let selectedView = UIView()
+        selectedView.backgroundColor = .red
+        selectedBackgroundView = selectedView
+    }
     
     public func bind(text: String) {
         self.lblMenu.text = text
+        backImage.isHidden = true
+    }
+    
+    public func showBackImage() {
+        backImage.isHidden = false
     }
     
 }
